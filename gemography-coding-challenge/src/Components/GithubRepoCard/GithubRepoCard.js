ea2d63de48ca466f42b135cc4dc8530c4590c4ba
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-09-30 21:49:21 -0300

    Created GithubRepoCard CSS file

---
+++ gemography-coding-challenge/src/Components/GithubRepoCard/GithubRepoCard.js
@@ -0,0 +1,12 @@
+import './GithubRepoCard.css';
+
+export default function GithubRepoCard() { 
+  
+    return (
+        <div className="box">
+            <h3>Title</h3>
+            <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png" alt="user-profile" className="image-box" />            
+            <h3>Description</h3>
+        </div>
+    );
+  }
