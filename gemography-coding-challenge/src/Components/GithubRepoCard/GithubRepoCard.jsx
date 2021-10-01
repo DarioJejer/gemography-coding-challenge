0aac59c127f632411345fdcf80f3985ddeda3a61
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-09-30 21:32:13 -0300

    Created GitubRepoCard

---
+++ gemography-coding-challenge/src/Components/GithubRepoCard/GithubRepoCard.jsx
@@ -0,0 +1,13 @@
+
+export default function GithubRepoCard() { 
+  
+    return (
+        <div className="box">
+            <h3>Title</h3>
+            <div className="image-box">
+                <p>Profile picture</p>
+            </div>
+            <h3>Description</h3>
+        </div>
+    );
+  }
