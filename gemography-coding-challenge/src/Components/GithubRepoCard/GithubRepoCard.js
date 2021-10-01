6eae5d52ef769b22cca82ef7e0cd62767ff9fcb9
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-10-01 01:14:57 -0300

    Added dinamic render of Repos

--- gemography-coding-challenge/src/Components/GithubRepoCard/GithubRepoCard.js
+++ gemography-coding-challenge/src/Components/GithubRepoCard/GithubRepoCard.js
@@ -3,12 +3,12 @@ import './GithubRepoCard.css';
 export default function GithubRepoCard(props) { 
   
     const {title, description, profileImg} = props;
-    
+
     return (
         <div className="box">
             <h3>{title}</h3>
             <img src={profileImg} alt="user-profile" className="image-box" />            
-            <h3>{description}</h3>
+            <p>{description}</p>
         </div>
     );
   }
--- gemography-coding-challenge/src/Components/GithubRepoCards/GithubRepoCards.js
+++ gemography-coding-challenge/src/Components/GithubRepoCards/GithubRepoCards.js
@@ -4,22 +4,20 @@ import {useEffect, useState} from "react";
 
 export default function GithubRepoCards() { 
     
-    const [repos, setRepos] = useState([
-            {title: "First repo", description: "This should appear at the top", profileImg: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"},
-            {title: "Second repo", description: "This should appear at the middle", profileImg: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"},
-            {title: "Third repo", description: "This should appear at the botton", profileImg: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"}
-        ]);
+    const [repos, setRepos] = useState([]);
 
-    useEffect(() => {        
+    useEffect(() => {   
+
         fetchData();
+
+        async function fetchData() {
+            let reposData = await fetch("https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc");
+            reposData = await reposData.json();
+            reposData = reposData.items;
+            setRepos(formatReposData(reposData));
+        }
     },[]);
     
-    async function fetchData() {
-        let reposData = await fetch("https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc");
-        reposData = await reposData.json();
-        reposData = reposData.items;
-        setRepos(formatReposData(reposData));
-    }
 
     function formatReposData(res) {
         return res.map(r => {return { title: r.name, description: r.description, profileImg: r.owner.avatar_url }});
@@ -27,9 +25,10 @@ export default function GithubRepoCards() {
 
     return (
         <div>
-            <GithubRepoCard title={repos[0].title} description={repos[0].description} profileImg={repos[0].profileImg}/>
-            <GithubRepoCard title={repos[1].title} description={repos[1].description} profileImg={repos[1].profileImg}/>
-            <GithubRepoCard title={repos[2].title} description={repos[2].description} profileImg={repos[2].profileImg}/>
+            {repos.length>0?
+            repos.map(r => <GithubRepoCard title={r.title} key={r.title} description={r.description} profileImg={r.profileImg}/>)
+            : <div>Loading...</div>
+            }
         </div>
     );
   }
