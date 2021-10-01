cc5b394d6a47b07e7e8526c31f986cda9fabe69c
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-10-01 02:26:40 -0300

    Moved LoadMoreRepos action dispatch to App

--- gemography-coding-challenge/src/App.js
+++ gemography-coding-challenge/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import GithubRepoCards from './Components/GithubRepoCards/GithubRepoCards';
+import {useEffect} from "react";
+import { LoadMoreRepos } from './redux/reducers/githubReposReducer/githubReposAction';
+import { useDispatch } from "react-redux";
 
 function App() {      
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {   
+    dispatch(LoadMoreRepos(1));
+  },[]);
 
   return (
     <div className="App">
       <GithubRepoCards/>
+      <button onClick={handleLoadMoreRepos}>Load more repos</button>
     </div>
   );
 }
--- gemography-coding-challenge/src/Components/GithubRepoCards/GithubRepoCards.js
+++ gemography-coding-challenge/src/Components/GithubRepoCards/GithubRepoCards.js
@@ -1,19 +1,12 @@
 import './GithubRepoCards.css';
 import GithubRepoCard from '../GithubRepoCard/GithubRepoCard';
-import {useEffect, useState} from "react";
-import { LoadMoreRepos } from '../../redux/reducers/githubReposReducer/githubReposAction';
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 
 export default function GithubRepoCards() { 
     
-    const dispatch = useDispatch();
     const repos = useSelector(state => state.githubReposReducer.githubRepos)
 
-    useEffect(() => {   
-        dispatch(LoadMoreRepos(1));
-    },[]);
-    
     if(repos.length>0){
         return (
             <div>
