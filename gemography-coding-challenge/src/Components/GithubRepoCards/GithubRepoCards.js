7f2b82682618a43c663a792de1bf1a3b9f3b2cae
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-09-30 23:18:28 -0300

    Implemented consumption of API data

--- gemography-coding-challenge/src/Components/GithubRepoCards/GithubRepoCards.js
+++ gemography-coding-challenge/src/Components/GithubRepoCards/GithubRepoCards.js
@@ -1,13 +1,29 @@
-import GithubRepoCard from '../GithubRepoCard/GithubRepoCard';
 import './GithubRepoCards.css';
+import GithubRepoCard from '../GithubRepoCard/GithubRepoCard';
+import {useEffect, useState} from "react";
 
 export default function GithubRepoCards() { 
     
-    let repos = [
-        {title: "First repo", description: "This should appear at the top", profileImg: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"},
-        {title: "Second repo", description: "This should appear at the middle", profileImg: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"},
-        {title: "Third repo", description: "This should appear at the botton", profileImg: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"}
-    ];
+    const [repos, setRepos] = useState([
+            {title: "First repo", description: "This should appear at the top", profileImg: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"},
+            {title: "Second repo", description: "This should appear at the middle", profileImg: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"},
+            {title: "Third repo", description: "This should appear at the botton", profileImg: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"}
+        ]);
+
+    useEffect(() => {        
+        fetchData();
+    },[]);
+    
+    async function fetchData() {
+        let reposData = await fetch("https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc");
+        reposData = await reposData.json();
+        reposData = reposData.items;
+        setRepos(formatReposData(reposData));
+    }
+
+    function formatReposData(res) {
+        return res.map(r => {return { title: r.name, description: r.description, profileImg: r.owner.avatar_url }});
+    }
 
     return (
         <div>
